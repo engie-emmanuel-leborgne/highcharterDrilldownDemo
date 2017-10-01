30881ccc025ac6fb5e4c21e2abfc26d8a00dfff7
kohleth <kohleth@gmail.com>
2017-10-01 20:11:11 +1100

    bar

--- demo.R
+++ demo.R
@@ -8,34 +8,66 @@ library(hts)
 X=aggts(infantgts,3)%>%
   as_data_frame()%>%
   mutate(Year=1933:2003)%>%
-  filter(Year>=2000)%>%
+  filter(Year>=1990)%>%
   gather(v,val,-Year)%>%
   separate(v,c("state","sex"),sep=" ")
 
 Total=X%>%
-  group_by(Year)%>%
-  summarize(val=sum(val))%>%
-  mutate(drilldown=Year)
+  rename(name=Year)%>%
+  group_by(name)%>%
+  summarize(y=sum(val))%>%
+  mutate(drilldown=name)%>%
+  arrange(desc(name))
   
 State=X%>%
   group_by(Year,state)%>%
-  summarize(val=sum(val))%>%
+  summarize(y=sum(val))%>%
   nest(-Year)%>%
-  mutate(data=map(data,list_parse))
+  mutate(data=map2(Year,data,function(id,d){
+    data=d%>%
+      mutate(drilldown=paste(id,state,sep="_"))%>%
+      select(name=state,y,drilldown)%>%
+      arrange(desc(y))%>%
+      list_parse()
+    list(name="State",id=id,data=data,type="bar")
+  }
+    )
+  )
 
 Sex=X%>%
+  mutate(id=paste(Year,state,sep="_"))%>%
+  nest(-id)%>%
+  mutate(data=map2(id,data,function(.id,d){
+    data=d%>%
+      select(name=sex,y=val)%>%
+      arrange(desc(y))%>%
+      list_parse()
+    list(name="Sex",id=.id,data=data,type="bar")
+  }
+  ))
 
-as.data.frame(infantgts)
-data=data_frame(
-  
-)
 
-h1list=lapply()
 
-h0=hchart(Total,"column",hcaes(y=val,x=Year,name="Year"))
-h0
-h0%>%
+h0=highchart() %>%
+  hc_title(text = "Multi-level Drilldown") %>%
+  hc_xAxis(type = "category") %>%
+  hc_legend(enabled = FALSE) %>%
+  hc_plotOptions(
+    series = list(
+      boderWidth = 0,
+      dataLabels = list(enabled = TRUE)
+    )
+  )%>%
+  hc_add_series(
+    data = Total,
+    name = "Year",
+    type = "bar"
+    )
+
+h0=h0%>%
   hc_drilldown(
     allowPointDrilldown=TRUE,
-    series=
+    series=c(State$data,Sex$data)
   )
+
+h0
